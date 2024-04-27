7406d8eb5250cc37d0b3de27950448e86759ad88
PamBWillenz <pam@positek.net>
2024-04-27 15:24:59 -0400

    set up react app for employees api

---
+++ src/components/employees/EmployeeList.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function EmployeeList() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/employees"
+        );
+        setEmployees(response.data);
+      } catch (error) {
+        console.log("Error fetching employees:", error);
+      }
+    };
+    fetchEmployees();
+  }, []);
+
+  return (
+    <div>
+      <h1>Employee List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Contact Number</th>
+            <th>Address</th>
+            <th>Pincode</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Date of Birth</th>
+            <th>Date of Hiring</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map(employee => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.first_name}</td>
+              <td>{employee.last_name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.contact_number}</td>
+              <td>{employee.address}</td>
+              <td>{employee.pincode}</td>
+              <td>{employee.city}</td>
+              <td>{employee.state}</td>
+              <td>{employee.date_of_birth}</td>
+              <td>{employee.date_of_hiring}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default EmployeeList;
