d1272e493d3be97bd63ca4f4268a9348deca0369
PamBWillenz <pam@positek.net>
2024-05-24 18:50:50 -0400

    add Edit compoment, consolidate with form component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EmployeeList from "./components/employees/EmployeeList";
 import EmployeeDetails from "./components/employees/EmployeeDetails";
 import EmployeeNewForm from "./components/employees/EmployeeNewForm";
+import EmployeeEditForm from "./components/employees/EmployeeEditForm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element=<EmployeeList /> />
         <Route path="/employee/:id" element=<EmployeeDetails /> />
         <Route path="/employee/new" element=<EmployeeNewForm /> />
+        <Route path="/employee/:id/edit" element=<EmployeeEditForm /> />
       </Routes>
     </Router>
   );
